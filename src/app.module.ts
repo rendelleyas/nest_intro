8b1870425060b671f9bca2c46483977c1905a8bf
Rendell Eyas <reyas@netlinkvoice.com>
2024-09-27 08:59:26 +0800

    up: re-module the database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,39 +5,10 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeviceModule } from './device/device.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-      expandVariables: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const dbHost = configService.get<string>('DB_HOST');
-        const dbPort = configService.get<number>('DB_PORT');
-        const dbUsername = configService.get<string>('DB_USERNAME');
-        const dbPassword = configService.get<string>('DB_PASSWORD');
-        const dbDatabase = configService.get<string>('DB_DATABASE');
-
-        console.log('dbDatabase', dbDatabase);
-        return {
-          type: 'mysql',
-          host: dbHost,
-          port: dbPort,
-          username: dbUsername,
-          password: dbPassword,
-          database: dbDatabase,
-          synchronize: true, // Use with caution in production
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        };
-      },
-    }),
-    UsersModule,
-    DeviceModule,
-  ],
+  imports: [DatabaseModule, UsersModule, DeviceModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const dbHost = configService.get<string>('DB_HOST');
+        const dbPort = configService.get<number>('DB_PORT');
+        const dbUsername = configService.get<string>('DB_USERNAME');
+        const dbPassword = configService.get<string>('DB_PASSWORD');
+        const dbDatabase = configService.get<string>('DB_DATABASE');
+
+        return {
+          type: 'mysql',
+          host: dbHost,
+          port: dbPort,
+          username: dbUsername,
+          password: dbPassword,
+          database: dbDatabase,
+          synchronize: true, // Use with caution in production
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
